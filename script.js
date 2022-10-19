0ba7b2843531a5e6a041fba631704af1c17b6e93
Damla Tabitha <104497858+DamlaTabitah@users.noreply.github.com>
2022-10-19 14:23:42 +0100

    Add files via upload
    practice

---
+++ script.js
@@ -0,0 +1,41 @@
+const obj = ([1, 2, 3, 4, 5]);
+let sum = 0;
+obj.forEach(num => {
+    sum += num;
+});
+console.log(sum);
+
+
+const obj = {a:1, b:2, C:3, e:4, f:5};
+let sum = 0;
+obj.forEach(num => {
+    sum += num;
+});
+console.log(sum);
+forEach();
+
+const obj = {
+    a: 1, 
+    b: 2,
+    C: 3,
+    e: 4, 
+    f: 5
+};
+
+let sum = 0;
+for ( let key in Object => {
+    return sum += obj[key];
+});
+const obj = {
+    a: 1, 
+    b: 2,
+    C: 3,
+    e: 4, 
+    f: 5
+};
+
+let sum = 0;
+for ( let key in Object => {
+    return sum += obj[key];
+});
+
